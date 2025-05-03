8f304b950cc1f23a3de996b277f7dba5957ec52d
DmKodoff <dmitrii.kot.off@gmail.com>
2025-05-03 10:53:47 +0300

    style: update responsive styles for Dashboard and Header components
    Adjust the order of elements in the Dashboard component for mobile view and improve the responsive layout of the Header component. This ensures a better user experience across different devices.

--- src/components/pages/Dashboard/index.tsx
+++ src/components/pages/Dashboard/index.tsx
@@ -5,11 +5,11 @@ import MotivationBanner from '@/components/features/MotivationBanner'
 import st from './Dashboard.module.scss'
 
 const Dashboard = () => (
-  <>
+  <div className={st.container}>
     <DashboardHeader className={st.header} />
     <LettersList className={st.list} />
     <MotivationBanner className={st.banner} />
-  </>
+  </div>
 )
 
 export default Dashboard
